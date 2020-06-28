3ad31aef5b672aedd48478604a456a5132339548
diana <dianbang@naver.com>
2020-06-28 17:23:15 +0900

    캘린더뷰 기능 추가

---
+++ app/src/main/java/com/example/finalexam/dataClass/certificationTestXmlData.kt
@@ -0,0 +1,25 @@
+package com.example.finalexam.dataClass
+
+class certificationTestXmlData() {
+//    <description>기능장(2020년도 제67회)</description>  회차
+//    <docexamdt>20200405</docexamdt>                   필기시험일자
+//    <docpassdt>20200410</docpassdt>                   필기시험 합격예정자 발표일자
+//    <docregenddt>20200313</docregenddt>               필기시험원서접수 종료일자
+//    <docregstartdt>20200310</docregstartdt>           필기원서 접수 시작일자
+//    <pracexamenddt>20200701</pracexamenddt>           면접시험 시작일자
+//    <pracexamstartdt>20200613</pracexamstartdt>       면접시험 종료일자
+//    <pracpassdt>20200710</pracpassdt>                 합격자 발표일자.
+//    <pracregenddt>20200414</pracregenddt>             면접시험 원서접수 종료일자.
+//    <pracregstartdt>20200413</pracregstartdt>         면접시험 원서접수 시작일자.
+
+    var description : String = ""
+    var docexamdt : String = ""
+    var docpassdt : String = ""
+    var docregenddt : String = ""
+    var docregstartdt : String = ""
+    var pracexamenddt : String = ""
+    var pracexamstartdt : String = ""
+    var pracpassdt : String = ""
+    var pracregenddt : String = ""
+    var pracregstartdt : String = ""
+}
